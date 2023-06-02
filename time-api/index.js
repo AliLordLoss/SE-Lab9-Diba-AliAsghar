62417f319540948eb267c5daa4c1816686da77bf
AliLordLoss <a.a.ghanati@gmail.com>
2023-06-02 20:22:51 +0330

    time-api project code is done

---
+++ time-api/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send(new Date());
+});
+
+app.get("/year", (req, res) => {
+  res.send(String(new Date().getUTCFullYear()));
+});
+
+app.get("/month", (req, res) => {
+  res.send(String(new Date().getUTCMonth() + 1));
+});
+
+app.get("/day", (req, res) => {
+  res.send(String(new Date().getUTCDate()));
+});
+
+app.get("/hour", (req, res) => {
+  res.send(String(new Date().getUTCHours()));
+});
+
+app.get("/minute", (req, res) => {
+  res.send(String(new Date().getUTCMinutes()));
+});
+
+app.get("/second", (req, res) => {
+  res.send(String(new Date().getUTCSeconds()));
+});
+
+app.get("/mili-second", (req, res) => {
+  res.send(String(new Date().getUTCMilliseconds()));
+});
+
+app.get("/timestamp", (req, res) => {
+  res.send(String(Date.now()));
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("404 - Not Found!");
+});
+
+app.listen(8000);
